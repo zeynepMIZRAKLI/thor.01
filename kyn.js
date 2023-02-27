04653d6d6c707328f315f0b048a082f388201564
ZEYNEP <123675933+zeynepMIZRAKLI@users.noreply.github.com>
2023-02-27 16:21:43 +0300

    thor

--- kyn.js
+++ kyn.js
@@ -5,5 +5,5 @@ function openNav() {
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%"; 
   }
